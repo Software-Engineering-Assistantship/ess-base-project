8530f5605ae742f173057ca2d36769996bccb835
cxmedeiros <cxm@cin.ufpe.br>
2023-08-16 03:22:41 -0300

    fit(search): created a search with the combnation of all filters

--- backend/src/service/impl/search.py
+++ backend/src/service/impl/search.py
@@ -2,27 +2,27 @@ from src.schemas.response import HTTPResponses, HttpResponseModel
 from src.schemas.album import AlbumCreateModel
 from src.db.__init__ import database as db
 
-# class FiltersService:
+class FiltersService:
 
-#     @staticmethod
-#     def get_filters(name: str):
-#         albuns = db.get_by_album('albuns', name)
-#         songs = db.get_by_album('songs', name)
-#         artists = [
-#         ]
+    @staticmethod
+    def get_filters(name: str):
+        albuns = db.get_by_album('albuns', name)
+        songs = db.get_by_album('songs', name)
+        artists = [
+        ]
 
-#         for album in albuns:
-#             artists.append(album['artist'])
+        for album in albuns:
+            artists.append(album['artist'])
         
-#         for song in songs:
-#             artists.append(song['artist'])
+        for song in songs:
+            artists.append(song['artist'])
         
-#         artists = list(set(artists))
+        artists = list(set(artists))
 
-#         albuns.extend(songs.extend(artists))
-#         return albuns
+        albuns.extend(songs.extend(artists))
+        return albuns
 
-#     @staticmethod
-#     def add_album(album: AlbumCreateModel):
-#         added_album = db.add('albuns', album)
-#         return added_album
+    @staticmethod
+    def add_album(album: AlbumCreateModel):
+        added_album = db.add('albuns', album)
+        return added_album
