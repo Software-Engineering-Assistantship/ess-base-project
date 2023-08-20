66f76b3d3c57846ba2fc0e8362efa19cd8274664
pedrocgr <pedroc.gr@hotmail.com>
2023-08-20 19:08:48 -0300

    change pytest

--- backend/src/tests/api/features/test_songs.py
+++ backend/src/tests/api/features/test_songs.py
@@ -6,12 +6,12 @@ def test_get_top_rated_songs(client: TestClient):
     # Iremos supor que temos 5 músicas no banco de dados, cada uma com uma avaliação média.
     mock_top_rated_songs = ["Song1", "Song2", "Song3", "Song4", "Song5"]
 
-    with patch.object(db, "get_top_rated_songs") as mock_get_top_rated:
+    with patch.object(db, "highlighted") as mock_get_top_rated:
         # A função mock retornará a lista de músicas mais bem avaliadas.
         mock_get_top_rated.return_value = mock_top_rated_songs
 
         # Iremos realizar a requisição para a rota que retorna as músicas mais bem avaliadas.
-        response = client.get("/songs/get_top_rated_songs")
+        response = client.get("/songs/get_highlighted")
 
     # Print debugging information
     print(f"Response Status: {response.status_code}")
