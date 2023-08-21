4764752436cc82016843f64343e3c8339fa47ec0
Kailane Felix <kailanefelx@gmail.com>
2023-08-21 15:02:45 -0300

    feat(test): add albums test

--- backend/src/tests/api/step_definitions/test_albums.py
+++ backend/src/tests/api/step_definitions/test_albums.py
@@ -31,6 +31,28 @@ def test_get_albums(client: TestClient):
         },
     ] }
 
+def test_get_multiple_albums(client: TestClient):
+    with patch.object(AlbumService, "get_albums") as mock_get_all_items:
+        mock_get_all_items.return_value = [
+            {
+                "id": "album1",
+                "title": "Album 1",
+                "artist": "Artist 1",
+                "release_year": 2019,
+            },
+            {
+                "id": "album2",
+                "title": "Album 2",
+                "artist": "Artist 2",
+                "release_year": 2020,
+            },
+        ]
+
+        response = client.get("/albums")
+
+    assert response.status_code == 200
+    assert len(response.json()['albums']) == 2
+
 def test_get_albums_empty_list(client: TestClient):
     with patch.object(AlbumService, "get_albums") as mock_get_all_items:
         mock_get_all_items.return_value = []  # Simulate empty list
