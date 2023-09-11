2f8217b0336bb5d22c00bbaa84ecf40e1ee4542f
Ian Gabriel <igbt@cin.ufpe.br>
2023-09-11 20:09:55 -0300

    small API lessons

--- frontend/src/views/testAPI.vue
+++ frontend/src/views/testAPI.vue
@@ -1,12 +1,30 @@
-<template>
-    <div>
+<script>
+import axios from 'axios';
 
-        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    </div>
-    <div>
-      <router-view></router-view>
-    </div>
-  </template>
+export default {
+  data () {
+    return{
+      posts: []
+    }
+  },
+  mounted(){
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+    .then(response => {
+      this.posts = response.data
+    })
+  }
+}
+</script>
+
+
+
+
+
+
+<template>
+  <h2>{{ posts.id }}{{ posts.title }}</h2>
+  <p>{{ posts.body }}</p>
+</template>
   
   <style scoped>
   *{
@@ -15,3 +33,4 @@
   }
   </style>
   
+
