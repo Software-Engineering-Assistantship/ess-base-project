2cb0cc636d6c263ead90a2829473a38b0bb396fb
bmmuc <bmmuc@cin.ufpe.br>
2023-09-11 23:49:31 -0300

    feat (most,inhigh,search): created integration with backend

--- frontend/src/shared/components/SearchFilterComponent/index.tsx
+++ frontend/src/shared/components/SearchFilterComponent/index.tsx
@@ -13,13 +13,25 @@ import {
 import filter from "../../assets/filter.png"; // Importando a imagem do ícone de filtro
 import Lupa from "../../assets/Lupa.png"; // Importando a imagem da lupa
 import axios from "axios";
+import MusicCard from "../MusicCard";
+import Error from "../Error";
 
 interface SearchResult {
   id: number;
   title: string;
   artist: string;
+  available_on: object;
+  image_uri: string;
+  popularity: number;
+  release_year: number;
 }
-
+interface ReponseTrue {
+  albums: SearchResult[];
+  songs: SearchResult[];
+}
+interface ResultReponse {
+  data: ReponseTrue[];
+}
 interface SearchFilterProps {
   onSearch: (query: string) => void;
   onFilter: () => void;
@@ -42,15 +54,16 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
 
   const handleSearch = async (e: React.FormEvent<HTMLInputElement>) => {
     const query = e.currentTarget.value;
-    onSearch(query); // Se ainda quiser usar onSearch para alguma lógica
-    await fetchData();
+    // onSearch(query); // Se ainda quiser usar onSearch para alguma lógica
+    setQueryText(query);
+    // await fetchData();
   };
 
   // Estado para controlar a visibilidade do dropdown
   const [showDropdown, setShowDropdown] = React.useState(false);
-
-  const [selectedGenre, setSelectedGenre] = React.useState('');
-  const [selectedYear, setSelectedYear] = React.useState('');
+  const [queryText, setQueryText] = React.useState('');
+  const [selectedGenre, setSelectedGenre] = React.useState('Nenhum');
+  const [selectedYear, setSelectedYear] = React.useState('Nenhum');
 
   const handleGenreChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedGenre(e.currentTarget.value);
@@ -59,19 +72,44 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
   const handleYearChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setSelectedYear(e.currentTarget.value);
   };
+  // response is a lsit of objects, create a interface for it
 
+  const handleResponse = (response: ResultReponse) => {
+    const aux = [];
+    response.albums.forEach((album) => {
+      album.image_url = 'https://akamai.sscdn.co/uploadfile/letras/fotos/5/2/6/6/5266e6a16b5fe4501de0d70cb2935f48.jpg'
+      aux.push(album);
+    }
+    );
+    response.songs.forEach((song) => {
+      song.image_url = 'https://akamai.sscdn.co/uploadfile/letras/fotos/5/2/6/6/5266e6a16b5fe4501de0d70cb2935f48.jpg'
+
+      aux.push(song);
+    }
+    );
+
+    setSearchResults(aux);
+  };
   const fetchData = async () => {
+    console.log(queryText);
+    console.log(selectedGenre);
+    console.log(selectedYear);
     try {
-      const response = await axios.get('/search', {
+      const response = await axios.get('http://127.0.0.1:8000/search/search', {
         params: {
-          query: searchQuery,
-          genre: selectedGenre,
-          year: selectedYear,
+          'name': queryText,
+          'genre': selectedGenre,
+         'year': Number(selectedYear),
         },
       });
       
       const data: SearchResult[] = response.data;
-      setSearchResults(data);
+      console.log('---------------');
+
+      console.log(data);
+      console.log('---------------');
+      handleResponse(response.data);
+      // setSearchResults(data);
     } catch (error) {
       console.error('Erro ao buscar dados:', error);
     }
@@ -86,7 +124,7 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
     <SearchMainDiv>
       <SearchFilterWrapper>
         {/* Imagem da lupa */}
-        <LupaImage src={Lupa} alt="lupa" />
+        <LupaImage src={Lupa} alt="lupa" onClick={() => fetchData()}/>
 
         {/* Input de pesquisa */}
         <SearchInput
@@ -110,6 +148,7 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
               <div style={{ marginBottom: "5px" }}></div>
               <select name="cars1" id="cars1" onChange={handleGenreChange}>
                 {/* Opções de gênero */}
+                <option value="Nenhum">Nenhum</option>
                 <option value="Pop">Pop</option>
                 <option value="MPB">MPB</option>
                 <option value="Rap">Rap</option>
@@ -134,21 +173,8 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
               <div style={{ marginBottom: "5px" }}></div>
               <label htmlFor="cars2">Ano de Lançamento</label>
               <div style={{ marginBottom: "5px" }}></div>
-              <select name="cars2" id="cars2"  onChange={handleYearChange}>
-                {/* Opções de ano de lançamento */}
-                <option value="Anos 80">Anos 60</option>
-                <option value="Anos 80">Anos 70</option>
-                <option value="Anos 80">Anos 80</option>
-                <option value="Anos 90">Anos 90</option>
-                <option value="2000-2004">2000-2004</option>
-                <option value="2005-2009">2005-2009</option>
-                <option value="2010-2014">2010-2014</option>
-                <option value="2015-2017">2015-2017</option>
-                <option value="2018-2020">2018-2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-                <option value="2023">2023</option>
-              </select>
+              <input type="number" name="cars2" id="cars2" onChange={handleYearChange}/>
+
             </div>
 
           </DropdownFilter>
@@ -158,19 +184,23 @@ const SearchFilterComponent: React.FC<SearchFilterProps> = ({
       <Results>
         {searchResults.length > 0 ? (
           <div>
-            <h2>Resultados da Pesquisa:</h2>
-            <ul>
-              {searchResults.map(result => (
+              {/* {searchResults.map(result => (
                 <ResultItem key={result.id}>
                   <strong>Título:</strong> {result.title} <br />
                   <strong>Artista:</strong> {result.artist}
-                  {/* Renderize outros campos conforme necessário */}
                 </ResultItem>
+              ))} */}
+              {searchResults.map(result => (
+                <MusicCard
+                  key={result.id}
+                  artist={result.artist}
+                  name={result.title}
+                  image={result.image_url}
+                />
               ))}
-            </ul>
           </div>
         ) : (
-          <div>Nenhum resultado encontrado</div>
+          <Error />
         )}
       </Results>
 
