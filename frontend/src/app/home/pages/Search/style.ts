d3b1e1fc23616b9410bf1a8af2346e4ef40ca621
cxmedeiros <cxm@cin.ufpe.br>
2023-09-12 00:04:11 -0300

    fix(Pages): creadded a new page for search the musics and albuns

---
+++ frontend/src/app/home/pages/Search/style.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wallpaper = styled.div`
+  background: linear-gradient(to bottom, #ff66c4, #ffde59);
+  min-height: 100vh; /* Isso garante que o gradiente cubra toda a altura da tela */
+`;
