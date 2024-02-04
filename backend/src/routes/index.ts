293a37d8d8fc93d17519fc58a3f2d1a791d88fc0
jrchakalo <jisj@cin.ufpe.br>
2024-02-04 16:35:04 -0300

    chore: add '/helloworld' to test server

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -7,6 +7,10 @@ const router = Router();
 const prefix = '/api';
 
 export default (app: Express) => {
+  app.get('/helloworld', (req, res) => { 
+    res.send('Hello World');
+  });
+
   app.use(
     prefix,
     new TestController(router, di.getService(TestService)).router
