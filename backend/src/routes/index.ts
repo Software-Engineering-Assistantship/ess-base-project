3d39aa9dfd495cdcc6924289f03d3aeb20ca4eb4
jrchakalo <jisj@cin.ufpe.br>
2024-02-06 15:08:51 -0300

    feat(user): add users backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,14 +2,19 @@ import { Express, Router } from 'express';
 import { di } from '../di';
 import TestController from '../controllers/test.controller';
 import TestService from '../services/test.service';
+import UserController from '../controllers/user.controller'; // Importa o controlador de usuário
+import UserService from '../services/user.service'; // Import the UserService class
 
 const router = Router();
+const userController = new UserController(router, di.getService(UserService));
 const prefix = '/api';
 
 export default (app: Express) => {
   app.get('/helloworld', (req, res) => { 
     res.send('Hello World');
   });
+
+  app.post(`${prefix}/cadastro`, (req, res) => userController.createUser(req, res));
 
   app.use(
     prefix,
