f168db80cb052985ccb360a3bdefa391e610cdf9
bfpimentac <beatriz.freire.pimenta@gmail.com>
2024-02-06 10:50:34 -0300

    fix(routes-delivery-person)): adding delivery-person to routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import UserRouter from './UserRoutes';
-
+import deliveryPersonRouter from './DeliveryPersonRoutes';
 const router = Router();
 
 router.use('/user', UserRouter);
-
+router.use('/deliveryperson', deliveryPersonRouter);
 router.route('/').get((_, res) => {
   res.status(200).send('🚀 ESS Server running');
 });
