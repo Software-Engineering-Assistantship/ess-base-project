9debb744c5688e2ee33a37020bed45fbb7a2fd61
jrchakalo <jisj@cin.ufpe.br>
2024-02-08 13:23:30 -0300

    chore: start new scenario implementation

--- backend/tests/controllers/users.steps.ts
+++ backend/tests/controllers/users.steps.ts
@@ -4,67 +4,123 @@ import app from '../../src/app';
 import { di } from '../../src/di';
 import UserRepository from '../../src/repositories/user.repository';
 import UserModel from '../../src/models/user.model';
+import fs from 'fs';
 
 const feature = loadFeature('tests/features/users.feature');
 const request = supertest(app);
 
 defineFeature(feature, (test) => {
-let mockUserRepository: UserRepository;
-let response: supertest.Response;
-const userData = new UserModel({
-    nome: '',
-    cpf: '',
-    dataNascimento: new Date(),
-    email: '',
-    login: '',
-    senha: ''
-});
-
-beforeEach(() => {
-    mockUserRepository = di.getRepository<UserRepository>(UserRepository);
-});
-
-  test('Cadastro de Usuário com Sucesso', ({ given, when, then, and }) => {
-    given(/^estou na página "(.*)"$/, async (page) => {
-        if (page == 'Cadastro de Usuário') {
-            page = 'api/users/cadastro';
-        }
-        const rota = '/${page}'
-        response = await request.get(rota);
+    let mockUserRepository: UserRepository;
+    let response: supertest.Response;
+    const userData = new UserModel({
+        nome: '',
+        cpf: '',
+        dataNascimento: new Date(),
+        email: '',
+        login: '',
+        senha: ''
     });
 
-    when(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
+    beforeEach(() => {
+        mockUserRepository = di.getRepository<UserRepository>(UserRepository);
     });
 
-    and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
-    });
+    test('Cadastro de Usuário com Sucesso', ({ given, when, then, and }) => {
+        given(/^estou na página "(.*)"$/, async (page) => {
+            if (page == 'Cadastro de Usuário') {
+                page = 'api/users/cadastro';
+            }
+            const rota = '/${page}'
+            response = await request.get(rota);
+        });
 
-    and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
-    });
+        when(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
 
-    and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
-    });
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
 
-    and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
-    });
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
 
-    and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
-        userData.preencherCampo(campo, valor);
-    });
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
 
-    and (/^realizo o cadastro do usuário$/, async () => {
-     response = await request.post('/api/users/cadastro').send(userData);
-    });
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
 
-    then(/^uma mensagem de confirmação é exibida indicando que "(.*)"$/, (expectedMessage) => {
-        console.log(response.body);
-        expect(response.status).toBe(200);
-        expect(response.body.msg).toBe(expectedMessage);
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and (/^realizo o cadastro do usuário$/, async () => {
+            response = await request.post('/api/users/cadastro').send(userData);
+        });
+
+        then(/^uma mensagem de confirmação é exibida indicando que "(.*)"$/, (expectedMessage) => {
+            expect(response.status).toBe(200);
+            expect(response.body.msg).toBe(expectedMessage);
+            fs.writeFileSync('users.json', JSON.stringify(userData));
+        });
     });
-  });
+    
+    /*test ('Falha no Cadastro de Usuário por Login já Cadastrado', ({ given, when, then, and }) => {
+        given(/^estou na página "(.*)"$/, async (page) => {
+            if (page == 'Cadastro de Usuário') {
+                page = 'api/users/cadastro';
+            }
+            const rota = '/${page}'
+            response = await request.get(rota);
+        });
+
+        and(/^existe um usuário cadastrado com o login "(.*)"$/, async (login) => {
+            const user = new UserModel({
+                nome: 'Teste',
+                cpf: '123.456.789-01',
+                dataNascimento: new Date('02/03/2000'),
+                email: 'teste@teste.com',
+                login: login,
+                senha: 'senhateste'
+            });
+        });       
+
+        when(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and(/^preencho o campo "(.*)" com "(.*)"$/, async (campo, valor) => {
+            userData.preencherCampo(campo, valor);
+        });
+
+        and (/^realizo o cadastro do usuário$/, async () => {
+        response = await request.post('/api/users/cadastro').send(userData);
+        });
+
+        then(/^uma mensagem de erro é exibida indicando que "(.*)"$/, (expectedMessage) => {
+            console.log(response.body);
+            expect(response.status).toBe(400);
+            expect(response.body.msg).toBe(expectedMessage);
+        });
+    });*/
 });
--- backend/tests/features/users.feature
+++ backend/tests/features/users.feature
@@ -13,3 +13,15 @@ Scenario: Cadastro de Usuário com Sucesso
     And preencho o campo "senha" com "SenhaSegura123"
     And realizo o cadastro do usuário
     Then uma mensagem de confirmação é exibida indicando que "O cadastro foi realizado com sucesso"
+
+Scenario: Falha no Cadastro de Usuário por Login já Cadastrado
+    Given estou na página "Cadastro de Usuário"
+    And o usuário de login "jorgelemos" está cadastrado no sistema
+    When preencho o campo "nome" com "Jorge Lemos"
+    And preencho o campo "cpf" com "143.416.189-06"
+    And preencho o campo "data de nascimento" com "08/08/1975 "
+    And preencho o campo "e-mail" com "jorgelemos@meuemail.com"
+    And preencho o campo "login" com "jorgelemos"
+    And preencho o campo "senha" com "SenhaSegura903"
+    And realizo o cadastro do usuário
+    Then uma mensagem de erro é exibida indicando que "O Login já está sendo utilizado"
