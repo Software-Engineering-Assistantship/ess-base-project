bdc9521584d5b008de430c572d8e2427563a3c5f
cebms <cemendes@gmail.com>
2024-02-12 09:50:14 -0300

    test(restaurant_registration): Implement first test using prismaMock

--- backend/tests/controllers/test.steps.ts
+++ backend/tests/controllers/test.steps.ts
@@ -1,48 +1,65 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
 import supertest from 'supertest';
 import app from '../../src/app';
-import { di } from '../../src/di';
-import TestRepository from '../../src/repositories/test.repository';
+import { prismaMock } from '../../setupTests';
+import { Restaurant } from '@prisma/client';
 
 const feature = loadFeature('tests/features/tests.feature');
 const request = supertest(app);
 
 defineFeature(feature, (test) => {
-  // mocking the repository
-  let mockTestRepository: TestRepository;
   let response: supertest.Response;
-
-  beforeEach(() => {
-    mockTestRepository = di.getRepository<TestRepository>(TestRepository);
-  });
-
-  test('Create a test', ({ given, when, then, and }) => {
-    given(/^o TestRepository não tem um test com nome "(.*)"$/, async (testId, testName) => {
-      // Check if the test does not exist in the repository and delete it if it exists
-      const existingTest = await mockTestRepository.getTest(testId);
-      if (existingTest) {
-        await mockTestRepository.deleteTest(testId);
-      }
-    });
-
-    when(
-      /^uma requisição POST for enviada para "(.*)" com o corpo da requisição sendo um JSON com o nome "(.*)"$/,
-      async (url, testName) => {
-        response = await request.post(url).send({
-          name: testName,
-        });
+  let restaurants: Restaurant[] = [];
+  test('Leitura de restaurantes do sistema', async ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    given(
+      /^existe um restaurante cadastrado no sistema com os dados "(.*)" "(.*)", email "(.*)" e senha "(.*)"$/,
+      async (name: string, cnpj: string, email: string, password: string) => {
+        restaurants.push({ id: 1, name, cnpj, email, password });
       }
     );
 
-    then(/^o status da resposta deve ser "(.*)"$/, (statusCode) => {
-      expect(response.status).toBe(parseInt(statusCode, 10));
+    and(
+      /^existe um restaurante cadastrado no sistema com os dados "(.*)" "(.*)", email "(.*)" e senha "(.*)"$/,
+      async (name: string, cnpj: string, email: string, password: string) => {
+        restaurants.push({ id: 2, name, cnpj, email, password });
+        prismaMock.restaurant.findMany.mockResolvedValue(restaurants);
+      }
+    );
+
+    when(/^uma requisição GET é enviada para "(.*)"$/, async (url) => {
+      response = await request.get(url);
     });
 
-    and(/^o JSON da resposta deve conter o nome "(.*)"$/, (testName) => {
-        expect(response.body.data).toEqual(
-          expect.objectContaining({
-            name: testName,
-          })
+    then(
+      /^é retornada uma mensagem com o status "(.*)"$/,
+      async (statusCode) => {
+        await expect(response.status).toBe(parseInt(statusCode, 10));
+      }
+    );
+
+    and(
+      /^a mensagem contém "(.*)", "(.*)", "(.*)"$/,
+      async (name, cnpj, email) => {
+        await expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ name, cnpj, email }),
+          ])
+        );
+      }
+    );
+
+    and(
+      /^a mensagem contém "(.*)", "(.*)", "(.*)"$/,
+      async (name, cnpj, email) => {
+        await expect(response.body).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({ name, cnpj, email }),
+          ])
         );
       }
     );
--- backend/tests/features/tests.feature
+++ backend/tests/features/tests.feature
@@ -1,8 +1,10 @@
 Feature: Tests
 
 # API
-Scenario: Create a test
-    Given o TestRepository não tem um test com nome "test"
-    When uma requisição POST for enviada para "/api/tests" com o corpo da requisição sendo um JSON com o nome "test"
-    Then o status da resposta deve ser "200"
-    And o JSON da resposta deve conter o nome "test" 
+Scenario: Leitura de restaurantes do sistema
+    Given existe um restaurante cadastrado no sistema com os dados "Quentinha refeições" "123321222", email "email_adm_restaurante" e senha "senha_adm_restaurante"
+    And existe um restaurante cadastrado no sistema com os dados "Guloso Trincado" "40028922", email "email_adm_restaurante_2" e senha "senha_adm_restaurante_2"
+    When uma requisição GET é enviada para "/restaurants"
+    Then é retornada uma mensagem com o status "200"
+    And a mensagem contém "Quentinha refeições", "123321222", "email_adm_restaurante"
+    And a mensagem contém "Guloso Trincado", "40028922", "email_adm_restaurante_2"
