41fc610e4958ba054e8b6a43a8ce6abdb2dbbda3
caiofernandes <caio.fern58@gmail.com>
2024-02-16 17:01:47 -0300

    feat(loginclient): migration.sql loginclient deleted

--- backend/prisma/migrations/20240209151206_loginclient/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `number` on the `Order` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Order` DROP COLUMN `number`,
-    ADD COLUMN `quantity` INTEGER NOT NULL DEFAULT 1;
