f9e8db8cc931fc638895c443252f038fd8018015
anasosl <asosl@cin.ufpe.br>
2024-02-18 11:10:08 -0300

    feat(*): send emails

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer")
+
+const sendEmail = async (subject, message, send_to) => {
+    const transporter = nodemailer.createTransport({
+        host: "smtp-mail.outlook.com", 
+        port: "587",
+        auth: {
+            user: "almocin.ess@outlook.com",
+            pass: "almocin.email"
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    })
+
+    const options = {
+        from: "almocin.ess@outlook.com",
+        to: send_to,
+        replyTo: "almocin.ess@outlook.com",
+        subject: subject,
+        html: message
+    }
+
+    transporter.sendMail(options, function(err, info){
+        if (err){
+            console.log(err)
+        } else {
+            console.log(info)
+        }
+    })
+}
+
+module.exports = sendEmail
