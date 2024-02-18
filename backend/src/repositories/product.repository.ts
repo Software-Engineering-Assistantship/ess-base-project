5a92104f70d69096be90c32cdc8916877c2c9b45
Leandro Lima <leandroluiz1906@gmail.com>
2024-02-17 22:31:13 -0300

    doc: add controller, model, repository and service files for product

---
+++ backend/src/repositories/product.repository.ts
@@ -0,0 +1,25 @@
+import ProductEntity from "../entities/product.entity";
+import ProductModel from "../models/product.model";
+import BaseRepository from "./base.repository";
+
+export class ProductRepository extends BaseRepository<ProductEntity> {
+  constructor() {
+    super('products');
+  }
+
+  public async getAllProducts(): Promise<ProductEntity[]> {
+    return await this.findAll();
+  }
+
+  public async getProductById(id: string): Promise<ProductEntity | null> {
+    return await this.findOne((product) => product.id === id);
+  }
+
+  public async createProduct(data: ProductEntity): Promise<ProductEntity> {
+    return await this.add(data);
+  }
+
+  public async updateProductById(id: string, data: ProductEntity): Promise<ProductEntity | null> {
+    return await this.update((product) => product.id === id, data);
+  }
+}
