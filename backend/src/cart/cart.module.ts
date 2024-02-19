367c73471bb10acd66af32bd102c27c1c4d5cb2b
Beatriz Férre <93141694+biaferre@users.noreply.github.com>
2024-02-19 16:03:54 -0300

    fix: provide service for cart module

--- backend/src/cart/cart.module.ts
+++ backend/src/cart/cart.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
 import { DatabaseModule } from 'src/database/database.module';
+import { CartService } from './services/cart.service'; 
 import { CartController } from './controllers/cart.controller';
 
 @Module({
   imports: [DatabaseModule],
+  providers: [PrismaService, CartService], 
   controllers: [
     CartController
   ],
