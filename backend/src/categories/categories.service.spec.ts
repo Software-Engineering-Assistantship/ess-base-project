1820fea5d6ef96c3c7ff6706d9faace037daed53
Caio Barreto <79955775+caiolbarreto@users.noreply.github.com>
2024-02-18 23:19:46 -0300

    fix: update categories service tests

--- backend/src/categories/categories.service.spec.ts
+++ backend/src/categories/categories.service.spec.ts
@@ -1,24 +1,45 @@
-import { describe, it, expect, beforeEach } from 'vitest';
+import { describe, it, expect, afterAll, beforeAll } from 'vitest';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { CategoriesService } from './categories.service';
 import { PrismaService } from 'src/database/prisma.service';
+import { RestaurantsService } from 'src/restaurants/services/restaurants.service';
 
 describe('CategoriesService', () => {
   let service: CategoriesService;
+  let restaurantService: RestaurantsService;
+
+  let firstRestaurantId: string;
   let firstCategoryId: string;
+
+  const mockRestaurant = {
+    name: 'Teste',
+    address: 'Rua',
+    closingTime: new Date(),
+    type: 'Sushi',
+  };
 
   const mockCategory = {
     name: 'Sushiiiiiiiiiiii',
     description: 'Great food',
+    restaurantId: '1',
   };
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoriesService, PrismaService],
+      providers: [CategoriesService, PrismaService, RestaurantsService],
     }).compile();
 
     service = module.get<CategoriesService>(CategoriesService);
+    restaurantService = module.get<RestaurantsService>(RestaurantsService);
+
+    const createdRestaurant = await restaurantService.create(mockRestaurant);
+    firstRestaurantId = createdRestaurant.id;
+    mockCategory.restaurantId = firstRestaurantId;
+  });
+
+  afterAll(async () => {
+    await restaurantService.remove(firstRestaurantId);
   });
 
   describe('create', () => {
@@ -28,11 +49,11 @@ describe('CategoriesService', () => {
       expect(createdCategory).toHaveProperty('id');
       expect(createdCategory).toHaveProperty('position');
 
-      expect(createdCategory['id']).toBeTypeOf('string');
-      expect(createdCategory['position']).toBeTypeOf('number');
+      expect(typeof createdCategory.id).toBe('string');
+      expect(typeof createdCategory.position).toBe('number');
 
-      expect(createdCategory['name']).toBe(mockCategory.name);
-      expect(createdCategory['description']).toBe(mockCategory.description);
+      expect(createdCategory.name).toBe(mockCategory.name);
+      expect(createdCategory.description).toBe(mockCategory.description);
 
       firstCategoryId = createdCategory.id;
     });
@@ -40,7 +61,8 @@ describe('CategoriesService', () => {
 
   describe('findAll', () => {
     it('should find all categories', async () => {
-      const categories = await service.findAll();
+      console.log('here', firstRestaurantId);
+      const categories = await service.findAll(firstRestaurantId);
 
       expect(categories).toBeTypeOf('object');
       expect(categories.length).toBeGreaterThan(0);
@@ -60,7 +82,10 @@ describe('CategoriesService', () => {
 
   describe('findByName', () => {
     it('should find a category by name', async () => {
-      const category = await service.findByName(mockCategory.name);
+      const category = await service.findByName(
+        mockCategory.name,
+        firstRestaurantId,
+      );
 
       expect(category).toHaveProperty('id', firstCategoryId);
       expect(category).toHaveProperty('name', mockCategory.name);
