c9c66097b32c6b0e360e35e33d55a142e9b1d92e
gui-maranhao <gmsmr@cin.ufpe.br>
2024-02-18 21:38:49 -0300

    feat(user): add user model

---
+++ backend/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true,
+    },
+
+    newPassword: {
+        type: String,
+        required: false
+    },
+
+    bio: {
+        type: String,
+        required: false
+    },
+
+    profileImage:{
+        type: String
+    },
+
+    coverImage:{
+        type: String
+    }
+    
+})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User
