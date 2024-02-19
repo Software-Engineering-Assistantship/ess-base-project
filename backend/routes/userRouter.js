aabf39b2af9e578cc10d0952f5dcff1e7fa6e004
gui-maranhao <gmsmr@cin.ufpe.br>
2024-02-19 01:49:11 -0300

    feat(user): removal of random functions

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -7,10 +7,6 @@ router.get('/', userController.getAll)
 
 router.get('/:id', userController.getUser)
 
-router.post('/create', userController.user_signup)
-
-router.post('/signin',userController.user_signin)
-
 router.put('/edit/:id', userController.updateUser)
 
 router.put('/editPass/:id', userController.updatePassword)
