4082602fd8aa5a2a8874b479f4979f712509d5d1
gui-maranhao <gmsmr@cin.ufpe.br>
2024-02-18 21:39:30 -0300

    feat(user): add user router

---
+++ backend/routes/userRouter.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+
+const userController = require("../controllers/userController")
+
+router.get('/', userController.getAll)
+
+router.get('/:id', userController.getUser)
+
+router.post('/create', userController.createUser)
+
+router.put('/edit/:id', userController.updateUser)
+
+router.put('/editPass/:id', userController.updatePassword)
+
+router.delete('/delete/:id', userController.deleteUser)
+
+module.exports = router
+
