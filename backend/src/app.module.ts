d5630edd0a8c53a2eaccbda94c7d512740882cff
Caio Barreto <79955775+caiolbarreto@users.noreply.github.com>
2024-02-18 23:15:01 -0300

    fix: include restaurant in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MenuModule } from './menu/menu.module';
 import { PaymentModule } from './payment/payment.module';
 import { CategoriesModule } from './categories/categories.module';
+import { RestaurantModule } from './restaurants/restaurant.module';
 import { OrderModule } from './orders/order.module';
 
 @Module({
@@ -11,6 +12,7 @@ import { OrderModule } from './orders/order.module';
     OrderModule,
     CategoriesModule,
     PaymentModule,
+    RestaurantModule,
     ConfigModule.forRoot(),
   ],
 })
