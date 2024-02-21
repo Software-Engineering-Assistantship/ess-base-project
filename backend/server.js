98f77dc990164cc9788295d16586b7ffb9be65d5
Maria Letícia Maranhão Nascimento <108680442+mleticiamn@users.noreply.github.com>
2024-02-21 03:35:23 -0300

    refactor: server

--- backend/server.js
+++ backend/server.js
@@ -3,16 +3,29 @@ const express = require('express')
 const mongoose = require('mongoose')
 require("dotenv").config()
 
+// import models
+const Restaurant = require('./models/Restaurant')
+const User = require('./models/User')
+const Forum = require('./models/Forum')
+const Rating = require('./models/Rating')
+const Review = require('./models/Review')
+
+// import controllers
+const RestaurantController = require('./controllers/restaurantController')
+const ForumController = require('./controllers/forumController')
+const UserController = require('./controllers/userController')
+const followersController = require('./controllers/followersController')
+const ReviewController = require('./controllers/reviewController')
+
 // import routers
 const restaurantRouter = require("./routes/restaurantRouter")
-const listRouter = require("./routes/listRouter")
-const searchesRouter = require("./routes/searchesRouter")
-const tendenciesRouter = require("./routes/tendenciesRouter")
 const forumRouter = require("./routes/forumRouter")
 const userRouter = require("./routes/userRouter")
 const followersRouter = require("./routes/followersRouter")
 const reviewRouter = require("./routes/reviewRouter")
 const ratingRouter = require("./routes/ratingRouter")
+const listRouter = require("./routes/listRouter")
+
 
 // use the PORT in .env or 3000 if it does not exist
 const port = process.env.PORT || 3000
@@ -25,10 +38,10 @@ app.use(express.json())
 
 // connect to data base
 const run = async () => {
-    await mongoose.connect(`mongodb://localhost:27017/${process.env.DBNAME}`);
+    await mongoose.connect(mongodb://localhost:27017/${process.env.DBNAME});
     console.log("Connected to data base")
 }
-    
+
 run()
 .catch((err) => console.error(err))
 
@@ -38,12 +51,9 @@ app.listen(port, () => console.log("Server started on port 3001"))
 
 // Routes
 app.use("/restaurants", restaurantRouter)
-app.use("/searches", searchesRouter)
-app.use("/tendencies", tendenciesRouter)
 app.use("/forum", forumRouter)
 app.use("/users", userRouter)
 app.use("/users", followersRouter)
 app.use("/reviews", reviewRouter)
 app.use("/ratings", ratingRouter)
 app.use("/lists", listRouter)
-
