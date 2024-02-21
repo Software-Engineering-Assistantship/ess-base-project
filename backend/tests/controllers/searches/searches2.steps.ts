dcc27727b1cd9bf9f74d8b9a176fd6ba3f2cb4e8
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-02-21 09:31:04 -0300

    Updating searches2.steps.ts

--- backend/tests/controllers/searches/searches2.steps.ts
+++ backend/tests/controllers/searches/searches2.steps.ts
@@ -1,9 +1,9 @@
 import { loadFeature, defineFeature } from "jest-cucumber"
 import axios, { AxiosResponse } from 'axios'
 const mongoose = require('mongoose')
-const Restaurant = require("../backend/models/Restaurant")
+const Restaurant = require("../../../models/Restaurant")
 
-const feature = loadFeature('tests/features/searches/searches1.feature');
+const feature = loadFeature('tests/features/searches/searches2.feature');
 
 const SERVER_URL = 'http://localhost:3001'
 
@@ -41,22 +41,21 @@ defineFeature(feature, test => {
     
 
     test("Busca por restaurante quando não há restaurantes cadastrados", ({ given, when, then, and }) => {
-        given(/^não existem restaurantes cadastrados no banco de dados"$/, async () => {
+        given('não existem restaurantes cadastrados no banco de dados', async () => {
             // Verifica se não há restaurantes cadastrados no banco de dados
             const restaurants = await Restaurant.find();
             expect(restaurants).toHaveLength(0);
         })
-        when(/^é feita uma requisição GET para "(.*)" com nome "(.*)"$/, async (path) => {
+        when(/^é feita uma requisição GET para "(.*)" com nome "(.*)"$/, async (path, name) => {
             try {
-                response = await axios.get(`${SERVER_URL}${path}`)
+                response = await axios.get(`${SERVER_URL}${path}`, { params: { name } });
             } catch (error) {
-                console.error('Error during HTTP request:', error)
                 return
             }
         })
-        then(/^o status de resposta deve ser "(.*)"$/, (status) => {
-            expect(String(response.status)).toBe(status)
-        })
+        then(/^o status da resposta deve ser "(.*)"$/, (arg0) => {
+            expect(String(response.status)).toBe(arg0);
+        });
         and(/^a resposta é "(.*)"$/, (ans) => { 
             expect(response.data).toEqual(ans)})
     })
