9e6a70b62e64984f2ad26255a1561e28ca374ee1
João Vitor Mergulhão <jvitormergulho@gmail.com>
2024-03-04 12:27:23 -0300

    feat(restaurants): add restaurant page basic structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
@@ -20,7 +19,7 @@ const router = createBrowserRouter([
         element: <Restaurants />,
       },
       {
-        path: "/restaurant/:id",
+        path: "/restaurants/:id",
         element: <RestaurantProfile />
       }
     ]
--- frontend/src/routes/Restaurants.js
+++ frontend/src/routes/Restaurants.js
@@ -1,9 +1,41 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import '../style/Restaurants.css'
+
+const API_BASE = "http://localhost:3001"
 
 const Restaurants = () => {
-    return (
-        <div> Restaurants </div>
-    )
-}
+    const [restaurants, setRestaurants] = useState([]);
+    const [error, setError] = useState(null);
 
-export default Restaurants
+    useEffect(() => {
+        GetRestaurants()
+            .catch(err => {
+                console.error("Error: ", err);
+                setError(err.message);
+            });
+    }, []); 
+
+    const GetRestaurants = async () => {
+        const response = await fetch(API_BASE + "/restaurants");
+        if (!response.ok) {
+            throw new Error("Failed to fetch");
+        }
+        const data = await response.json();
+        setRestaurants(data);
+    };
+
+    return (
+        <div className="restaurant-list"> 
+            <h2 className="category"> Categoria: Todos </h2>
+            {error && <p>Error: {error}</p>}
+            {restaurants.map(restaurant => (
+                <div className="restaurant-preview" key={restaurant.id}> 
+                    <h2>{restaurant.name}</h2>
+                    <p>{restaurant.address.neighborhood} - {restaurant.address.city}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Restaurants;
