a5f14fdb804b717f232b478c9fc8aad8d2c39d39
liviabion <llb@cin.ufpe.br>
2024-03-05 09:25:34 -0300

    feat(dev): promotion list modal

--- frontend/src/app/(admin)/categorias/page.tsx
+++ frontend/src/app/(admin)/categorias/page.tsx
@@ -1,11 +1,12 @@
 import { Button } from '@/components/ui/button'
 import  PromotionComponent  from '@/components/promotions/promotionComponent'
+import { ModalPromotion } from '@/components/promotions/promotionComponent'
 
 
 export default function Home() {
     return (
       <main>
-        <PromotionComponent category="Eletrônicos" discout="10%" end_date="10/10/2021" />
+        <ModalPromotion />
       </main>
     )
   }
