abd068c0f936088e3a5e57ee332ae8c1984c633f
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-03-09 22:31:55 -0300

    Update Header.js

--- frontend/src/header/Header.js
+++ frontend/src/header/Header.js
@@ -1,22 +1,34 @@
 import React from "react"
-import '../style/Feed.css'
+import { useState, useEffect } from "react"
+import '../style/Header.css'
 import logo from "../assets/logo.svg";
 import noprofileimage from "../assets/noprofileimage.png";
-const API_BASE = "http://localhost:3001"
 
-const Feed = () => {
+const Header = () => {
+    const [restaurant, setRestaurant] = useState("");
+
     return (
     <div> 
         <div className ="header">
             <img src={logo} alt="logo" className="logo"/>
+
             <h1 className="restaurants">Restaurantes</h1>
             <h1 className="users">Usuários</h1>
             <h1 className="lists">Listas</h1>
             <h1 className="forum">Fórum</h1>
+
+            <input 
+                type="text" 
+                placeholder="Ache seus pontos gastronômicos favoritos" 
+                value={restaurant} 
+                onChange={(e) => setRestaurant(e.target.value)} 
+                className="input-field"
+            />
+
             <img src={noprofileimage} alt="noprofileimage" className="noprofileimage"/>
         </div>
     </div>
     );
 }
 
-export default Header
+export default Header;
