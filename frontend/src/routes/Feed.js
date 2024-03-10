e95e4da3f4152e5d3e077012c55d591d84f9ba10
Rebeca Menezes <112202209+RebecaMenezess@users.noreply.github.com>
2024-03-10 00:19:52 -0300

    Update Feed.js

--- frontend/src/routes/Feed.js
+++ frontend/src/routes/Feed.js
@@ -5,11 +5,21 @@ const API_BASE = "http://localhost:3001"
 
 const Feed = () => {
     return (
-        // renders the header
         <div>
-            <Header />
-        </div>    
-    );
+            <div className ="header">
+                <Header />
+            </div>
+
+            <h1 className="trendingReviews">Reviews em Alta</h1>
+            <div className="listTrendingReviews"></div>
+
+            <h1 className="randomRestaurants">Restaurantes</h1>
+            <div className="listRandomRestaurants"></div>
+
+            <h1 className="randomReviews">Reviews</h1>
+            <div className="listRandomReviews"></div>
+        </div>
+    )
 }
 
 export default Feed
