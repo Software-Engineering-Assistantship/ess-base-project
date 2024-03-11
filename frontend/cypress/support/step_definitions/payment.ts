d36d8042824aaf53154399f0983f68e4e0f104dd
Clara Kenderessy <claraabk1@gmail.com>
2024-03-10 23:44:33 -0300

    feat: payment tests

--- frontend/cypress/support/step_definitions/payment.ts
+++ frontend/cypress/support/step_definitions/payment.ts
@@ -2,9 +2,12 @@
 
 import { Given, When, Then } from '@badeball/cypress-cucumber-preprocessor'
 
+
+// Scenario: Criar forma de pagamento
 Given('que o usuário está na página {string}', (url: string) => {
   cy.visit(url)
 })
+
 
 Given('não existe um cartão com final {string}', (categoryName: string) => {
   cy.contains(categoryName).should('not.exist')
@@ -40,5 +43,75 @@ Then(
   'o usuário deve ver os últimos dígitos {string} na lista de cartões',
   (categoryName: string) => {
     cy.contains(categoryName).should('exist')
+  },
+)
+
+// Scenario: Criar forma de pagamento com mesmo número
+Given('existe um cartão com os 4 últimos dígitos {string}', (cardNumber: string) => {
+  cy.contains(cardNumber).should('exist')
+})
+
+Then(
+  'o usuário deve receber uma mensagem de erro com o texto {string}',
+  (errorMessage: string) => {
+    cy.on('window:alert', (text) => {
+      expect(text).to.equal(errorMessage)
+    })
+  },
+)
+
+
+// Scenario: Editar forma de pagamento
+Given('que o usuário não admin está na página {string}', (url: string) => {
+  cy.visit(url)
+})
+
+When(
+  'o usuário clica no símbolo de editar cartão do cartão {string}',
+  (cardNumber: string) => {
+    cy.contains('div', cardNumber)
+      .parent()
+      .find('[aria-label="Editar"]')
+      .click()
+  },
+)
+
+When(
+  'o usuário atualiza o campo {string} com o número {string}',
+  (label1: string, value1: string) => {
+    cy.contains('label', label1).next().type(value1)
+  },
+)
+
+When('o usuário clica no botão para salvar {string}', (buttonText: string) => {
+  cy.contains('button', buttonText).click()
+})
+
+Then(
+  'o usuário deve ver o número {string} na lista de cartões',
+  (cardNumber: string) => {
+    cy.contains(cardNumber).should('exist')
+  },
+)
+
+// Scenario: Deletar cartão
+When(
+  'o usuário clica no símbolo de deletar cartão do cartão {string}',
+  (cardNumber: string) => {
+    cy.contains('div', cardNumber)
+      .parent()
+      .find('[aria-label="Excluir"]')
+      .click()
+  },
+)
+
+When('o usuário clica no botão de confirmação {string}', (buttonText: string) => {
+  cy.contains('button', buttonText).click()
+})
+
+Then(
+  'o usuário não deve ver o cartão de número {string} na lista de cartões',
+  (cardNumber: string) => {
+    cy.contains(cardNumber).should('not.exist')
   },
 )
