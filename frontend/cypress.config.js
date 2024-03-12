9a5ef2bbfa721a791aac7fc99ed4ac3a3b5c7165
jvrco <jvrco@cin.ufpe.br>
2024-03-12 20:39:43 -0300

    feat(user): add users profile picture in the users page

---
+++ frontend/cypress.config.js
@@ -0,0 +1,38 @@
+const { defineConfig } = require("cypress");
+const cucumberPreprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+module.exports = defineConfig({
+  video: false,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  defaultCommandTimeout: 5000,
+  execTimeout: 60000,
+  retries: {
+    runMode: 1,
+    openMode: 0,
+  },
+  chromeWebSecurity: false,
+  env: {
+    codeCoverage: {
+      exclude: ["cypress/**/*.*", "coverage/**/*.*"],
+    },
+  },
+  e2e: {
+    async setupNodeEvents(on, config) {
+      await cucumberPreprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)],
+      }));
+
+      // TODO: Fix coverage
+      // coverageTask(on, config);
+
+      return config;
+    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.feature",
+  },
+});
--- frontend/src/routes/Users.js
+++ frontend/src/routes/Users.js
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import '../style/Users.css';
-import logo from "../assets/logo.svg";
-
+import '../style/Users.css'; 
+import noprofileimage from '../assets/noprofileimage.png'; 
 const API_BASE = "http://localhost:3001";
 
 const Users = () => {
     const [users, setUsers] = useState([]);
-    const [searchTerm, setSearchTerm] = useState(''); // Search term state
+    const [searchTerm, setSearchTerm] = useState('');
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(true);
 
@@ -16,19 +15,12 @@ const Users = () => {
         const fetchUsers = async () => {
             try {
                 const response = await axios.get(`${API_BASE}/users`);
-                setUsers(response.data);
+                setUsers(response.data); // Assuming response.data is an array of user objects
                 setError('');
             } catch (error) {
-                if (error.response) {
-                    console.error('Error fetching users:', error.response.data);
-                    setError('Failed to fetch users: ' + error.response.data.message);
-                } else if (error.request) {
-                    console.error('Error fetching users:', error.request);
-                    setError('No response received.');
-                } else {
-                    console.error('Error message:', error.message);
-                    setError('Error: ' + error.message);
-                }
+                // Handle error
+                console.error('Error fetching users:', error);
+                setError('Failed to fetch users.');
             } finally {
                 setLoading(false);
             }
@@ -37,8 +29,7 @@ const Users = () => {
         fetchUsers();
     }, []);
 
-    // Filter users based on the search term
-    const filteredUsers = users.filter(user => 
+    const filteredUsers = users.filter(user =>
         user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
         user.email.toLowerCase().includes(searchTerm.toLowerCase())
     );
@@ -52,21 +43,29 @@ const Users = () => {
                     placeholder="Search users..." 
                     className="search-bar"
                     value={searchTerm}
-                    onChange={e => setSearchTerm(e.target.value)} // Update search term based on user input
+                    onChange={e => setSearchTerm(e.target.value)}
                 />
                 {loading && <div className="loading">Carregando Usuários...</div>}
                 {error && <div className="error-message">{error}</div>}
                 <ul className='alluserinfo'>
-                    {filteredUsers.map(user => ( // Use filteredUsers here
-                        <li key={user._id} className='infouser'>
-                            <div className="user-details">
-                                <h2>{user.name}</h2>
-                                <p>{user.email}</p>
-                            </div>
-                            <Link to={`/users/${user._id}`} className='buttonuserpage'>Ver Perfil</Link>
-                        </li>
+                    {filteredUsers.map(user => (
+                    <li key={user._id} className='infouser'>
+                    <div className="user-image-container">
+                        <img 
+                            src={user.profileImage ? `${API_BASE}/${user.profileImage}` : noprofileimage} 
+                            alt={`${user.name}'s profile`} 
+                            className="user-image"
+                        />
+                    </div>
+                    <div className="user-details">
+                        <h2>{user.name}</h2>
+                        <p>{user.email}</p>
+                    </div>
+                    <Link to={`/users/${user._id}`} className='buttonuserpage'>Ver Perfil</Link>
+                    </li>
                     ))}
                 </ul>
+
             </div>
         </div>
     );
