2d4a38faa6cf263019051e949fcc4b61b77b6895
gui-maranhao <gmsmr@cin.ufpe.br>
2024-03-12 15:24:14 -0300

    feat(user): add token verification to user edit and user page

--- frontend/src/routes/ProfileCard.js
+++ frontend/src/routes/ProfileCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Form, useNavigate, useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 import '../style/ProfileCard.css'
 import noProfileImage from "../images/noprofileimage.png"
 import noCoverImage from "../images/nocoverimage.png"
@@ -10,7 +10,8 @@ import axios from "axios";
 const API_BASE = "http://localhost:3001"
 
 const ProfileCard = () => {
-
+    const clickCover = useRef(null);
+    const clickProfile = useRef(null);
     const navigate = useNavigate()
 
     const [user, setUser] = useState(null);
@@ -37,12 +38,14 @@ const ProfileCard = () => {
     }, []); 
 
     
-    if(user && user.profileImage) {
-        noProfileImage = user.profileImage;
+    let check1 = false;
+    if(user && !user.profileImage) {
+        check1 = true;    
     }
-    
-    if(user && user.coverImage) {
-        noCoverImage = user.coverImage;
+   
+    let check2 = false;
+    if(user && !user.coverImage) {
+        check2 = true;
     }
     
     if(user && !user.bio){
@@ -73,23 +76,44 @@ const ProfileCard = () => {
         });
     };
 
+    const handleCoverChange = () => {
+        clickCover.current.click();
+    };
+
+    const handleProfilePicChange = () => {
+        clickProfile.current.click();
+    };
     
     return( user ? (
         <div class="profilecard">
             <div class="coverContainer">
-                <img class="coverimage" src={`${API_BASE}/${user.coverImage}` || noCoverImage}></img>
+                {
+                    check2 ? (
+                        <img class="coverimage" src={noCoverImage}></img>
+                    ) : (
+                        <img class="coverimage" src={`${API_BASE}/${user.coverImage}`}></img>
+                    )
+                }
                 <div class="coverContainer2">
                     <p class="coverfrase">Trocar Capa</p>
-                    <input class="botaocapa" type="file" id="coverPhoto" name="file2"
-                        onChange={ev => setFiles2(ev.target.files)}/>
+                    <button class="botaocapa" src={iconPencil} onClick={handleCoverChange}></button>
+                    <input type="file" id="coverPhoto" name="file2" style={{display: 'none'}}
+                        ref={clickCover} onChange={ev => setFiles2(ev.target.files)}/>
                 </div>
             </div>
             <div class="perfilcontainer">
-                <img class="profileimage" src={`${API_BASE}/${user.profileImage}` || noProfileImage}></img>
+                {
+                    check1 ? (
+                        <img class="profileimage" src={noProfileImage}></img>
+                    ) : (
+                        <img class="profileimage" src={`${API_BASE}/${user.profileImage}`}></img>
+                    )
+                }
                 <div class="perfilcontainer2">
                     <p class="perfilfrase">Trocar Ícone</p>
-                    <input class="botaoperfil" type="file" id="profilePhoto" name="file1"
-                        onChange={ev => setFiles1(ev.target.files)}/>
+                    <button class="botaoperfil" src={iconPencil} onClick={handleProfilePicChange}></button>
+                    <input type="file" id="profilePhoto" name="file1" style={{display: 'none'}}
+                        ref={clickProfile} onChange={ev => setFiles1(ev.target.files)}/>
                 </div>
             </div>
             <div class="nomecontainer">
